7ec0a03878f389b1caa20d4cef2619287a85ccfc
vinnylinck <vinnylinck@gmail.com>
2014-05-09 17:07:29 -0300

    Removing dialogs plugin

--- demo/IngenicoApp/platforms/ios/www/cordova_plugins.js
+++ demo/IngenicoApp/platforms/ios/www/cordova_plugins.js
@@ -6,20 +6,12 @@ module.exports = [
         "clobbers": [
             "window.tlantic.plugins.ismp"
         ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
-        "id": "org.apache.cordova.dialogs.notification",
-        "merges": [
-            "navigator.notification"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.tlantic.plugins.ismp": "0.1.0",
-    "org.apache.cordova.dialogs": "0.2.7"
+    "com.tlantic.plugins.ismp": "0.1.0"
 }
 // BOTTOM OF METADATA
 });
--- demo/IngenicoApp/platforms/ios/www/plugins/com.tlantic.plugins.ismp/www/ismp.js
+++ demo/IngenicoApp/platforms/ios/www/plugins/com.tlantic.plugins.ismp/www/ismp.js
@@ -1,43 +1,17 @@
-cordova.define("com.tlantic.plugins.ismp.iSMP", function(require, exports, module) {
-               /* global module, require */
-               'use strict';
-               
-               var exec = require('cordova/exec');
-               
-               // secure copy definition
-               function iSMP(){
-                    this.pluginRef = 'iSMP';                              // *** Plugin reference for Cordova.exec calls
-                    this.connectionEvent = 'iSMP_CON_STATUS_CHANGE';
-               }
-               
-               // connect-load method
-               iSMP.prototype.load = function (successCallback, errorCallback) {
-                    exec(successCallback, errorCallback, this.pluginRef, 'loadTerminal', []);
-               };
-               
-               
-               // return connection status
-               iSMP.prototype.isConnected =  function (successCallback, errorCallback) {
-                    exec(successCallback, errorCallback, this.pluginRef, 'isTerminalConnected', []);
-               };
-               
-               // return terminal status
-               iSMP.prototype.getStatus =  function (successCallback, errorCallback) {
-                    exec(successCallback, errorCallback, this.pluginRef, 'getTerminalStatus', []);
-               };
-               
-               // triggers connection changes
-               iSMP.prototype.yieldConnectionChange = function (connected) {
-                    var status = (connected === "YES"),
-                        ev = document.createEvent('Events');
-               
-                        ev.initEvent(this.connectionEvent, true, true);
-                        ev.status = status;
-               
-                        document.dispatchEvent(ev);
-               };
-               
-               
+cordova.define("com.tlantic.plugins.ismp.iSMP", function(require, exports, module) { /* global module, require */
+'use strict';
+
+var exec = require('cordova/exec');
+
+// secure copy definition
+function iSMP(){
+    this.pluginRef = 'iSMP';                              // *** Plugin reference for Cordova.exec calls
+}
+
+// connect method
+iSMP.prototype.load = function (successCallback, errorCallback) {
+	exec(successCallback, errorCallback, this.pluginRef, 'loadTerminal', []);
+};
+
 module.exports = new iSMP();
-               
 });
